14d8ad19d7c9041b9df11741bcfa676d530aa113
Abstr4ction <92829772+Abstr4ction@users.noreply.github.com>
2022-11-18 00:00:48 -0600

    Added Code Skeleton

--- Scrapped Space/main.lua
+++ Scrapped Space/main.lua
@@ -3,5 +3,10 @@
 -- main.lua
 -- TEST Group 9
 -----------------------------------------------------------------------------------------
-
 -- Your code here
+widget = require( "widget" )
+local composer = require("composer")
+csv = require("csv")
+display.setStatusBar(display.HiddenStatusBar)
+
+composer.gotoScene("Menu")
